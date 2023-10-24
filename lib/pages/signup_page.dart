c305848957f3d9e43822bc5ddd0bdd28d7a218e6
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 10:40:17 +0800

    add logo

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -16,26 +16,35 @@ class SignUpPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to WalletWise!',
-              style: theme.textTheme.headlineSmall!.copyWith(
-                fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                width: 80,
+                height: 80,
               ),
-            ),
-            Text(
-              'Where Your Financial Journey Begins',
-              style: theme.textTheme.titleSmall!.copyWith(
-                fontWeight: FontWeight.bold,
-                color: Colors.black.withOpacity(0.25),
+              const SizedBox(height: 16),
+              Text(
+                'Welcome to WalletWise!',
+                style: theme.textTheme.headlineSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+              Text(
+                'Where Your Financial Journey Begins',
+                style: theme.textTheme.titleSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black.withOpacity(0.25),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
