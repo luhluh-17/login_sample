be94756b88e7f77fcb10555a38c0d4bee1e56eb6
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 09:17:05 +0800

    add routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_sample/constants/routes.dart';
+import 'package:login_sample/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +17,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      initialRoute: Routes.login,
+      routes: {
+        Routes.login: (context) => const LoginPage(),
+      },
     );
   }
 }
