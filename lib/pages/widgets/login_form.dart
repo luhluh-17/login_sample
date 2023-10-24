8583f4a0e484e02029a81265fdc5835e1a17df3c
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 10:14:22 +0800

    add padding in form

--- lib/pages/widgets/login_form.dart
+++ lib/pages/widgets/login_form.dart
@@ -10,23 +10,26 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Username',
-              suffixIcon: Icon(Icons.person_2_outlined),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                suffixIcon: Icon(Icons.person_2_outlined),
+              ),
             ),
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              suffixIcon: Icon(Icons.lock_outline_rounded),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                suffixIcon: Icon(Icons.lock_outline_rounded),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
