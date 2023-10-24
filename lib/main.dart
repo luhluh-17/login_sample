145aab2f447051a6397956e2204deaf0a68b8111
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 11:01:22 +0800

    add providerscope in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:login_sample/constants/routes.dart';
 import 'package:login_sample/pages/login_page.dart';
 import 'package:login_sample/pages/signup_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
