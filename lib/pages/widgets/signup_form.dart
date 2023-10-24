79c438d99d617fc7d96993f7ccd311eb461745f0
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 10:47:24 +0800

    create signup form widget

---
+++ lib/pages/widgets/signup_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  State<SignupForm> createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
