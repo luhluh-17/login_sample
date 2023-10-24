8a21002da2a5e474ffa2e82eda0a6d3858be07a7
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 10:35:02 +0800

    add welcome text

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -5,6 +5,31 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to WalletWise!',
+              style: theme.textTheme.headlineSmall!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Where Your Financial Journey Begins',
+              style: theme.textTheme.titleSmall!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.black.withOpacity(0.25),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
