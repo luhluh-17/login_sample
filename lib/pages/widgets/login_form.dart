01a22eb624d4bc96c3fab97dc7d8f84dcc8b41c0
luhluh-17 <castro.ralph17@gmail.com>
2023-10-24 10:13:05 +0800

    create login form widget

---
+++ lib/pages/widgets/login_form.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Username',
+              suffixIcon: Icon(Icons.person_2_outlined),
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              suffixIcon: Icon(Icons.lock_outline_rounded),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
