56912996a5ad5817e3ebd588763caa630a40b782
luhluh-17 <castro.ralph17@gmail.com>
2023-11-07 11:20:02 +0800

    add card title

--- lib/widgets/pie_chart_container.dart
+++ lib/widgets/pie_chart_container.dart
@@ -6,36 +6,49 @@ class PieChartContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: PieChart(
-            PieChartData(sections: [
-              PieChartSectionData(
-                color: Colors.purple[100],
-                value: 40,
-                title: 'Food',
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Expense Structure',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
               ),
-              PieChartSectionData(
-                color: Colors.purple[200],
-                value: 20,
-                title: 'Transportation',
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              height: 200,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: PieChart(
+                PieChartData(sections: [
+                  PieChartSectionData(
+                    color: Colors.purple[100],
+                    value: 40,
+                    title: 'Food',
+                  ),
+                  PieChartSectionData(
+                    color: Colors.purple[200],
+                    value: 20,
+                    title: 'Transportation',
+                  ),
+                  PieChartSectionData(
+                    color: Colors.purple[300],
+                    value: 25,
+                    title: 'Online Shopping',
+                  ),
+                  PieChartSectionData(
+                    color: Colors.purple[400],
+                    value: 5,
+                    title: 'Investment',
+                  ),
+                ]),
               ),
-              PieChartSectionData(
-                color: Colors.purple[300],
-                value: 25,
-                title: 'Online Shopping',
-              ),
-              PieChartSectionData(
-                color: Colors.purple[400],
-                value: 5,
-                title: 'Investment',
-              ),
-            ]),
-          ),
+            ),
+          ],
         ),
       ),
     );
