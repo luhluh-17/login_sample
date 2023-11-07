bcd7398ecefdd840eb804f0cabc66cfb0c305f2f
luhluh-17 <castro.ralph17@gmail.com>
2023-11-07 14:02:55 +0800

    add value

--- lib/widgets/deposit_dialog.dart
+++ lib/widgets/deposit_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:login_sample/models/account.dart';
+import 'package:login_sample/providers/account_provider.dart';
+import 'package:login_sample/providers/balance_provider.dart';
 
 final depositProvider = StateProvider<double>((_) => 0.0);
 
@@ -13,6 +16,9 @@ class DepositDialog extends ConsumerStatefulWidget {
 class _DepositDialogState extends ConsumerState<DepositDialog> {
   @override
   Widget build(BuildContext context) {
+    final amount = ref.watch(depositProvider);
+    final account = ref.watch(accountProvider);
+
     return AlertDialog(
       title: const Text(
         'Enter Amount',
@@ -24,12 +30,17 @@ class _DepositDialogState extends ConsumerState<DepositDialog> {
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value) {
-              ref.read(depositProvider.notifier).state = double.parse(value);
+              double amount = double.parse(value);
+              ref.read(depositProvider.notifier).state = amount;
             },
           ),
           const SizedBox(height: 16.0),
           FilledButton(
-            onPressed: () {},
+            onPressed: () {
+              ref.read(balanceProvider.notifier).state =
+                  ref.watch(balanceProvider) + amount;
+              Navigator.pop(context);
+            },
             child: const Text('Deposit'),
           ),
         ],
