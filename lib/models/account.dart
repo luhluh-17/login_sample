4ba0b2f24c7bd2177e32e81853dd617dfd83c8eb
luhluh-17 <castro.ralph17@gmail.com>
2023-11-07 13:02:49 +0800

    add method to convert to json

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -3,4 +3,12 @@ class Account {
   final String password;
 
   Account({required this.username, required this.password});
+
+  // Add method to convert to json
+  Map<String, dynamic> toJson() {
+    return {
+      'username': username,
+      'password': password,
+    };
+  }
 }
